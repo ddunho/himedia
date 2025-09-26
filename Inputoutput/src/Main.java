0e667a5385902bdd891409feba70cce470be6f91
ddunho <dydkwogur@naver.com>
2025-09-26 09:16:14 +0900

    font

--- Inputoutput/src/Main.java
+++ Inputoutput/src/Main.java
@@ -1,12 +1,5 @@
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        try(FileWriter filewriter = new FileWriter("output.txt")){
-            filewriter.write("Hello World");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        System.out.println(Day.MONDAY);
     }
 }
