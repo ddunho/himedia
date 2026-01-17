ad0d83faee5793bafd47a0931911b2eaab4c48ea
ddunho <dydkwogur@naver.com>
2026-01-17 20:47:00 +0900

    array

---
+++ JavaScript/array.js
@@ -0,0 +1,16 @@
+// 첫번째 인덱스 0
+let myArray = [1,2,3,4,5];
+
+console.log(myArray[2]); 
+// 3 출력
+// push : 맨 뒤에 삽입
+myArray.push(6);
+console.log(myArray); 
+
+// pop : 맨 뒤에 제거
+myArray.pop();
+console.log(myArray); 
+
+//length : 배열 안에 갯수
+console.log(myArray.length);
+
