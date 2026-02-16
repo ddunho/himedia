108bb42fd4e606675c3dc46e004fc9066da950f8
ddunho <dydkwogur@naver.com>
2026-02-16 19:00:17 +0900

    destructuring

---
+++ JavaScript/destructuring.js
@@ -0,0 +1,13 @@
+//디스트럭처링 할당문법
+
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+
+const {name,age,city} = person;
+
+console.log(name);
+console.log(age);
+console.log(city);
